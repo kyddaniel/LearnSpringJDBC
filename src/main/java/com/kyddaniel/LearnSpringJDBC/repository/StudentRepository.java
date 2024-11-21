b43e7a96b5d789f656b347dee2f2a091521e8d36
kyddaniel <mksuenaa@connect.ust.hk>
2024-11-21 22:59:35 +0800

    Implement a JDBC Template

--- src/main/java/com/kyddaniel/LearnSpringJDBC/repository/StudentRepository.java
+++ src/main/java/com/kyddaniel/LearnSpringJDBC/repository/StudentRepository.java
@@ -1,6 +1,8 @@
 package com.kyddaniel.LearnSpringJDBC.repository;
 
 import com.kyddaniel.LearnSpringJDBC.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,8 +11,23 @@ import java.util.List;
 @Repository
 public class StudentRepository {
 
+    private JdbcTemplate jdbc;
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
     public void save(Student student) {
-        System.out.println("added");
+
+        String sql = "insert into student (rollno, name, marks) values (?,?,?)";
+
+        int rows = jdbc.update(sql, student.getRollNo(), student.getName(), student.getMarks());
+        System.out.println(rows + " effected");
     }
 
     public List<Student> findAll() {
