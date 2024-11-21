4896517d6d43003fd5d726e1b3fa92a2a3ddbe3d
kyddaniel <mksuenaa@connect.ust.hk>
2024-11-21 22:34:18 +0800

    Create a Student class

--- src/main/java/com/kyddaniel/LearnSpringJDBC/LearnSpringJdbcApplication.java
+++ src/main/java/com/kyddaniel/LearnSpringJDBC/LearnSpringJdbcApplication.java
@@ -1,13 +1,24 @@
 package com.kyddaniel.LearnSpringJDBC;
 
+import com.kyddaniel.LearnSpringJDBC.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LearnSpringJdbcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringJdbcApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(LearnSpringJdbcApplication.class, args);
+
+		Student student = context.getBean(Student.class);
+		student.setRollNo(1);
+		student.setName("Alice");
+		student.setMarks(69);
+
+		addStudent(student);
 	}
+
 
 }
